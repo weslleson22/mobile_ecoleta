8ee9a0c19fa41710ef07d6904a48324f8bf8a288
weslleson22 <wesllesonsowza@gmail.com>
2020-07-08 19:26:36 -0300

    barra de navergação

--- App.tsx
+++ App.tsx
@@ -6,7 +6,9 @@ import {AppLoading} from 'expo';
 import {Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto';
 import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu';
 
-import Home from './src/pages/Home';
+import Routes from './src/routes';
+
+//import Home from './src/pages/Home';
 export default function App() {
   const [fontsLoaded]=useFonts({
     Roboto_400Regular,
@@ -22,7 +24,7 @@ export default function App() {
   return (
    <>
      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-     <Home/>
+     <Routes/>
         </>
   );
 }
